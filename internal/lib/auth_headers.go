af974f3a5d28e7370b358bd0a18883b5f1fd6a6e
Andrew Snow <andrew.snow@nullify.cloud>
2024-09-05 16:54:52 +1000

    support a list of headers for dast scans

--- internal/lib/auth_headers.go
+++ internal/lib/auth_headers.go
@@ -11,16 +11,19 @@ func ParseAuthHeaders(authHeaders []string) (map[string]string, error) {
 	result := map[string]string{}
 
 	for _, header := range authHeaders {
-		headerParts := strings.Split(header, ": ")
-		if len(headerParts) != 2 {
-			err := fmt.Errorf("please provide headers in the format of 'key: value'")
-			logger.Err(err)
-			return nil, err
-		}
+		headers := strings.Split(header, ",")
+		for _, h := range headers {
+			headerParts := strings.Split(h, ": ")
+			if len(headerParts) != 2 {
+				err := fmt.Errorf("please provide headers in the format of 'key: value'")
+				logger.Err(err)
+				return nil, err
+			}
 
-		headerName := strings.TrimSpace(headerParts[0])
-		headerValue := strings.TrimSpace(headerParts[1])
-		result[headerName] = headerValue
+			headerName := strings.TrimSpace(headerParts[0])
+			headerValue := strings.TrimSpace(headerParts[1])
+			result[headerName] = headerValue
+		}
 	}
 
 	return result, nil
